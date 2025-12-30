a23eb95fd3073874c7db7a9c00990a31aa3ef463
Agneay B Nair <agneaybnair@gmail.com>
2025-12-30 16:28:55 +0530

    added remaining files

--- ws5/eight.hs
+++ ws5/eight.hs
@@ -1,5 +1,12 @@
 {-
-Create Your Own for Loop. 
-Write a higher-order function forLoop that mimics the behavior of an imperative for loop. 
- forLoop :: Int -> Int -> (Int -> IO ()) -> IO () 
+Create Your Own for Loop.
+Write a higher-order function forLoop that mimics the behavior of an imperative for loop.
+ forLoop :: Int -> Int -> (Int -> IO ()) -> IO ()
 -}
+
+forLoop :: Int -> Int -> (Int -> IO ()) -> IO ()
+forLoop start end body
+  | start > end = return ()
+  | otherwise = do
+      body start
+      forLoop (start + 1) end body
--- ws5/ten.hs
+++ ws5/ten.hs
@@ -1,7 +1,7 @@
 {-
-Implement Infinite List Generation (iterate). Write a function myIterate that generates an infinite list by repeatedly applying a function to an initial value. 
-myIterate :: (a -> a) -> a -> [a] 
+Implement Infinite List Generation (iterate). Write a function myIterate that generates an infinite list by repeatedly applying a function to an initial value.
+myIterate :: (a -> a) -> a -> [a]
 -}
 
-myIterate :: (a->a)->a->[a]
+myIterate :: (a -> a) -> a -> [a]
 myIterate f x = x : myIterate f (f x)
